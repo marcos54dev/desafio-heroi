39d115242e4d76825d55655e5c6b7af6c0e3f40f
marcos38dev <marcosoliveira38@hotmail.com>
2023-11-10 11:55:37 -0300

    criação do desafio do heroi

---
+++ index.js
@@ -0,0 +1,33 @@
+let nomeHeroi = "Marcos";
+
+let qtdExperiencia = 1001;
+
+
+if (qtdExperiencia <= 1000) {
+  let nivel = "Ferro";
+  console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
+
+} else if (qtdExperiencia >= 1001 && qtdExperiencia <= 2000) {
+  let nivel = "Bronze";
+  console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
+} else if (qtdExperiencia >= 2001 && qtdExperiencia <= 5000) {
+  let nivel = "Prata";
+  console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
+} else if (qtdExperiencia >= 6001 && qtdExperiencia <= 7000) {
+  let nivel = "Ouro";
+  console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
+} else if (qtdExperiencia >= 7001 && qtdExperiencia <= 8000) {
+  let nivel = "Platina";
+  console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
+} else if (qtdExperiencia >= 8001 && qtdExperiencia <= 9000) {
+  let nivel = "Ascendente";
+  console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
+} else if (qtdExperiencia >= 9001 && qtdExperiencia <= 10000) {
+  let nivel = "Imortal";
+  console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
+} else {
+  let nivel = "Radiante";
+  console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
+};
+
+  
